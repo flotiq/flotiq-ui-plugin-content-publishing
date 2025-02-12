ae12b0588fa8b6414638dae28bf04b04fc808d43
Krzysztof Brylski <krzysztof.brylski@codewave.pl>
2025-02-12 21:18:16 +0100

    #25914 added usage of public argument in flotiq plugin api

--- common/api-helpers.js
+++ common/api-helpers.js
@@ -1,9 +1,7 @@
 const cachedPublicVersions = {};
 
 export const getPublicVersion = (client, object) => {
-  // @todo return object based on header public, not version
-  // it requires to add in flotiq client method like getPublic(), listPublic etc. (or getDraft())
-  const publicVersion = object?.internal?.publicVersion;
+  const status = object?.internal?.status;
   const type = object?.internal?.contentType;
   const id = object?.id;
 
@@ -11,23 +9,19 @@ export const getPublicVersion = (client, object) => {
     return null;
   }
 
-  if (!publicVersion || publicVersion < 0) {
+  if (!["modified", "draft"].includes(status)) {
     return null;
   }
 
-  const cachedVersion = cachedPublicVersions[type]?.[id]?.[publicVersion];
+  const cachedVersion = cachedPublicVersions[type]?.[id]?.[status];
   if (cachedVersion) return cachedVersion;
 
   if (!cachedPublicVersions[type]) cachedPublicVersions[type] = {};
   if (!cachedPublicVersions[type][id]) cachedPublicVersions[type][id] = {};
 
-  cachedPublicVersions[type][id][publicVersion] = client[type]
-    .getVersion(id, publicVersion)
-    .then(({ body }) => (
-      body.code === 200
-        ? body
-        : object
-    ));
+  cachedPublicVersions[type][id][status] = client[type]
+    .get(id, true)
+    .then(({ body }) => (body.code === 200 ? body : object));
 
-  return cachedPublicVersions[type][id][publicVersion];
+  return cachedPublicVersions[type][id][status];
 };
